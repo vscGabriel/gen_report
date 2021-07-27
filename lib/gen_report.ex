cb8800a50a561e486bccec96eb69603b9544b61c
vscGabriel <gvscceca@gmail.com>
2021-07-27 00:39:44 -0300

    aaaaaa

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,18 +1,97 @@
 defmodule GenReport do
-  @moduledoc """
-  Documentation for `GenReport`.
-  """
+  alias GenReport.Parser
 
-  @doc """
-  Hello world.
+  @people [
+    "Cleiton",
+    "Daniele",
+    "Danilo",
+    "Diego",
+    "Giuliano",
+    "Jakeliny",
+    "Joseph",
+    "Mayk",
+    "Rafael",
+    "Vinicius"
+  ]
 
-  ## Examples
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
 
-      iex> GenReport.hello()
-      :world
+  @years [
+    2016,
+    2017,
+    2018,
+    2019,
+    2020
+  ]
 
-  """
-  def hello do
-    :world
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn data, report -> sum_values(data, report) end)
+  end
+
+  defp sum_values(
+         [name, hours, _, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         }
+       ) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+    hours_per_month = Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
+    hours_per_year = Map.put(hours_per_year[name], year, hours_per_year[name][year] + year)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp report_acc() do
+    %{
+      "all_hours" => generate_all_hours_map(),
+      "hours_per_month" => generate_hours_per_month_map(),
+      "hours_per_year" => generate_hours_per_year_map()
+    }
+  end
+
+  defp generate_all_hours_map do
+    @people
+    |> Enum.into(%{}, fn elem -> {elem, 0} end)
+  end
+
+  defp generate_hours_per_month_map do
+    @people
+    |> Enum.into(%{}, fn elem -> {elem, create_month_hours()} end)
+  end
+
+  defp generate_hours_per_year_map do
+    @people
+    |> Enum.into(%{}, fn elem -> {elem, create_year_hours()} end)
+  end
+
+  defp create_year_hours do
+    @years
+    |> Enum.into(%{}, fn elem -> {elem, 0} end)
+  end
+
+  defp create_month_hours do
+    @months
+    |> Enum.into(%{}, fn elem -> {elem, 0} end)
   end
 end
---
+++ lib/parser.ex
@@ -0,0 +1,38 @@
+defmodule GenReport.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(&parse_line(&1))
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim("\n")
+    |> String.split(",")
+    |> cast_numbers()
+  end
+
+  defp cast_numbers([name, worked_hours, day, month, year]) do
+    [
+      name,
+      String.to_integer(worked_hours),
+      String.to_integer(day),
+      parse_month(month),
+      String.to_integer(year)
+    ]
+  end
+
+  defp parse_month(month) when month == "1", do: "janeiro"
+  defp parse_month(month) when month == "2", do: "fevereiro"
+  defp parse_month(month) when month == "3", do: "março"
+  defp parse_month(month) when month == "4", do: "abril"
+  defp parse_month(month) when month == "5", do: "maio"
+  defp parse_month(month) when month == "6", do: "junho"
+  defp parse_month(month) when month == "7", do: "julho"
+  defp parse_month(month) when month == "8", do: "agosto"
+  defp parse_month(month) when month == "9", do: "setembro"
+  defp parse_month(month) when month == "10", do: "outubro"
+  defp parse_month(month) when month == "11", do: "novembro"
+  defp parse_month(month) when month == "12", do: "dezembro"
+  defp parse_month(_month), do: "invalid month"
+end
